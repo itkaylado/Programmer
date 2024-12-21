22ab1d9d36c4279bb3d1685ee83e854702523b75
itklado <kladesuyi@outlook.com>
2024-12-21 15:20:04 -0700

    Commiting main and gitignore

---
+++ main.py
@@ -0,0 +1,21 @@
+def print_my_name():
+    print("My name is Drogba")
+
+if __name__ == "__main__":
+    print_my_name()
+
+
+def print_my_name():
+    name = 'Stinger'
+    print(f"My name is {name}")
+
+if __name__ == "__main__":
+    print_my_name()
+
+
+def print_my_name():
+    name = input("Type your name: ")
+    print(f"My name is {name}")
+
+if __name__ == "__main__":
+    print_my_name()
